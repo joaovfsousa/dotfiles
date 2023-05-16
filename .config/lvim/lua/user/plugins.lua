a9237fd8c2b72b1d38c5bdb00cb6e149135e9cfa
João Vitor Sousa <joaovfsousa@gmail.com>
2023-05-16 15:51:35 -0300

    setup copilot

--- .config/lvim/lua/user/plugins.lua
+++ .config/lvim/lua/user/plugins.lua
@@ -13,15 +13,46 @@ lvim.plugins = {
     dependencies = {
       { "nvim-lua/plenary.nvim" },
       { "nvim-treesitter/nvim-treesitter" }
-    }
+    },
+    config = function()
+      require('refactoring').setup({})
+    end
   },
   {
-    "lewis6991/gitsigns.nvim"
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require('gitsigns').setup()
+    end
   },
   {
-    "axelvc/template-string.nvim"
+    "axelvc/template-string.nvim",
+    config = function()
+      require('template-string').setup({
+        remove_template_string = true
+      })
+    end
   },
   {
-    "norcalli/nvim-colorizer.lua"
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require('colorizer').setup()
+    end
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+      })
+    end,
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    config = function()
+      require("copilot_cmp").setup()
+    end
   }
 }
