645682037b3d56b0d652f8c0606f86dc698da6e0
João Vitor Sousa <joaovfsousa@gmail.com>
2023-06-24 03:55:14 -0300

    setup lua lsp

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,14 +1,15 @@
-reload('user.keymaps')
-reload('user.options')
-reload('user.plugins')
-reload('user.plugins-config')
+reload("user.keymaps")
+reload("user.options")
+reload("user.plugins")
+reload("user.plugins-config")
+reload("user.lsp")
 
-require('dap.ext.vscode').load_launchjs(nil, { node = { 'typescript', 'typescriptreact' } })
+require("dap.ext.vscode").load_launchjs(nil, { node = { "typescript", "typescriptreact" } })
 
 vim.api.nvim_create_autocmd({ "ColorScheme" }, {
-  pattern = "*",
-  callback = function()
-    vim.api.nvim_set_hl(0, "LineNr", { fg = "#888888" })
-    vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#dddddd" })
-  end
+	pattern = "*",
+	callback = function()
+		vim.api.nvim_set_hl(0, "LineNr", { fg = "#888888" })
+		vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#dddddd" })
+	end,
 })
