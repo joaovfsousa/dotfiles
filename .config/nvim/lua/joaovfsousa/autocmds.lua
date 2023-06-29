c1d15e7cb249983fcff7c63eb3d4341eef971f77
João Vitor Sousa <joaovfsousa@gmail.com>
2023-06-29 01:30:20 -0300

    add highlight on yank

---
+++ .config/nvim/lua/joaovfsousa/autocmds.lua
@@ -0,0 +1,29 @@
+local definitions = {
+  {
+    "TextYankPost",
+    {
+      group = "_general_settings",
+      pattern = "*",
+      desc = "Highlight text on yank",
+      callback = function()
+        vim.highlight.on_yank({ higroup = "Search", timeout = 100 })
+      end,
+    },
+  },
+}
+
+function define_autocmds(definitions)
+  for _, entry in ipairs(definitions) do
+    local event = entry[1]
+    local opts = entry[2]
+    if type(opts.group) == "string" and opts.group ~= "" then
+      local exists, _ = pcall(vim.api.nvim_get_autocmds, { group = opts.group })
+      if not exists then
+        vim.api.nvim_create_augroup(opts.group, {})
+      end
+    end
+    vim.api.nvim_create_autocmd(event, opts)
+  end
+end
+
+define_autocmds(definitions)
