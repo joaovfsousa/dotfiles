3e1dfe73a656c7981c09fabb25d75007af868d60
João Vitor Sousa <joaovfsousa@gmail.com>
2023-07-15 14:43:23 -0300

    disable wezterm panel navigation

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,38 +2,38 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
 
-local function isViProcess(pane)
-  return pane:get_foreground_process_name():find("n?vim") ~= nil
-end
-
-local function conditionalActivatePane(
-  window,
-  pane,
-  pane_direction,
-  vim_direction
-)
-  if isViProcess(pane) then
-    window:perform_action(
-      act.SendKey({ key = vim_direction, mods = "CTRL" }),
-      pane
-    )
-  else
-    window:perform_action(act.ActivatePaneDirection(pane_direction), pane)
-  end
-end
-
-wezterm.on("ActivatePaneDirection-right", function(window, pane)
-  conditionalActivatePane(window, pane, "Right", "l")
-end)
-wezterm.on("ActivatePaneDirection-left", function(window, pane)
-  conditionalActivatePane(window, pane, "Left", "h")
-end)
-wezterm.on("ActivatePaneDirection-up", function(window, pane)
-  conditionalActivatePane(window, pane, "Up", "k")
-end)
-wezterm.on("ActivatePaneDirection-down", function(window, pane)
-  conditionalActivatePane(window, pane, "Down", "j")
-end)
+-- local function isViProcess(pane)
+--   return pane:get_foreground_process_name():find("n?vim") ~= nil
+-- end
+--
+-- local function conditionalActivatePane(
+--   window,
+--   pane,
+--   pane_direction,
+--   vim_direction
+-- )
+--   if isViProcess(pane) then
+--     window:perform_action(
+--       act.SendKey({ key = vim_direction, mods = "CTRL" }),
+--       pane
+--     )
+--   else
+--     window:perform_action(act.ActivatePaneDirection(pane_direction), pane)
+--   end
+-- end
+--
+-- wezterm.on("ActivatePaneDirection-right", function(window, pane)
+--   conditionalActivatePane(window, pane, "Right", "l")
+-- end)
+-- wezterm.on("ActivatePaneDirection-left", function(window, pane)
+--   conditionalActivatePane(window, pane, "Left", "h")
+-- end)
+-- wezterm.on("ActivatePaneDirection-up", function(window, pane)
+--   conditionalActivatePane(window, pane, "Up", "k")
+-- end)
+-- wezterm.on("ActivatePaneDirection-down", function(window, pane)
+--   conditionalActivatePane(window, pane, "Down", "j")
+-- end)
 
 local config = {}
 
@@ -41,7 +41,7 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-config.leader = { key = "a", mods = "CTRL" }
+config.leader = { key = "b", mods = "CTRL" }
 
 -- config.initial_rows = 36
 -- config.initial_cols = 148
@@ -65,26 +65,26 @@ config.keys = {
       one_shot = false,
     }),
   },
-  {
-    key = "h",
-    mods = "CTRL",
-    action = act.EmitEvent("ActivatePaneDirection-left"),
-  },
-  {
-    key = "j",
-    mods = "CTRL",
-    action = act.EmitEvent("ActivatePaneDirection-down"),
-  },
-  {
-    key = "k",
-    mods = "CTRL",
-    action = act.EmitEvent("ActivatePaneDirection-up"),
-  },
-  {
-    key = "l",
-    mods = "CTRL",
-    action = act.EmitEvent("ActivatePaneDirection-right"),
-  },
+  -- {
+  --   key = "h",
+  --   mods = "CTRL",
+  --   action = act.EmitEvent("ActivatePaneDirection-left"),
+  -- },
+  -- {
+  --   key = "j",
+  --   mods = "CTRL",
+  --   action = act.EmitEvent("ActivatePaneDirection-down"),
+  -- },
+  -- {
+  --   key = "k",
+  --   mods = "CTRL",
+  --   action = act.EmitEvent("ActivatePaneDirection-up"),
+  -- },
+  -- {
+  --   key = "l",
+  --   mods = "CTRL",
+  --   action = act.EmitEvent("ActivatePaneDirection-right"),
+  -- },
   {
     key = "\\",
     mods = "LEADER",
