46a25c1dbeff61ca3857e7962d9c634def4f5d40
João Vitor Sousa <joaovfsousa@gmail.com>
2023-12-24 19:17:40 -0300

    update wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,39 +2,6 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
 
--- local function isViProcess(pane)
---   return pane:get_foreground_process_name():find("n?vim") ~= nil
--- end
---
--- local function conditionalActivatePane(
---   window,
---   pane,
---   pane_direction,
---   vim_direction
--- )
---   if isViProcess(pane) then
---     window:perform_action(
---       act.SendKey({ key = vim_direction, mods = "CTRL" }),
---       pane
---     )
---   else
---     window:perform_action(act.ActivatePaneDirection(pane_direction), pane)
---   end
--- end
---
--- wezterm.on("ActivatePaneDirection-right", function(window, pane)
---   conditionalActivatePane(window, pane, "Right", "l")
--- end)
--- wezterm.on("ActivatePaneDirection-left", function(window, pane)
---   conditionalActivatePane(window, pane, "Left", "h")
--- end)
--- wezterm.on("ActivatePaneDirection-up", function(window, pane)
---   conditionalActivatePane(window, pane, "Up", "k")
--- end)
--- wezterm.on("ActivatePaneDirection-down", function(window, pane)
---   conditionalActivatePane(window, pane, "Down", "j")
--- end)
-
 local config = {}
 
 if wezterm.config_builder then
@@ -49,7 +16,7 @@ config.leader = { key = "b", mods = "CTRL" }
 -- config.initial_cols = 148
 
 config.color_scheme = "Dracula (Official)"
-config.window_background_opacity = 0.92
+config.window_background_opacity = 0.8
 
 config.font_size = 15
 config.line_height = 1
