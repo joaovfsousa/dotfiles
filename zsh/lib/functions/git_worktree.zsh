1162fae7b70f56ad8c660ef53fdc0d124f32513d
João Vitor Sousa <joaovfsousa@gmail.com>
2024-02-19 22:10:41 -0300

    zsh(git-worktree): add method

--- zsh/lib/functions/git_worktree.zsh
+++ zsh/lib/functions/git_worktree.zsh
@@ -22,8 +22,53 @@ function worktree_env {
   fi 
 }
 
-function worktree_add {
-  echo $@
+function _get_tree_name_from_branch {
+  input=$1
+
+  if [[ $input == *"/"* ]]; then
+    before_slash=${input%%/*}
+
+    after_slash=${input#*/}
+    after_slash=${after_slash//[!0-9]/}
+
+    input="${before_slash}${after_slash}"
+  fi
+
+  echo $input
+}
+
+
+function worktree_add { 
+  new_branch_input=$1
+  base_branch_input=$2
+
+  if [[ $# -eq 0 ]]; then
+    echo "Enter the name of the new branch: \c"
+    IFS= read -r new_branch_input
+  fi
+
+  tree_name=$(_get_tree_name_from_branch $new_branch_input)
+
+  if git rev-parse --verify --quiet "$new_branch_input"; then
+    git worktree add $tree_name $new_branch_input
+    return 0
+  fi
+
+  if [[ $# -lt 2 ]]; then
+    base_branch_input=$(git branch --format='%(refname:short)' | fzf --header-first --header $'Select the base branch') 
+  fi
+
+  if [[ -z $base_branch_input ]]; then
+    echo "Base branch not selected"
+    return 1
+  fi
+
+  if ! git show-ref --quiet "$base_branch_input"; then
+    echo "Branch '$base_branch_input' does not exists."
+    return 1
+  fi
+
+  git worktree add -b $new_branch_input --checkout $tree_name $base_branch_input
 }
 
 function worktree {
