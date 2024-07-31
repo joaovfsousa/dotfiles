a4cd1221ed0b2cf2a1ba36fa83af0fdd682e96bb
João Vitor Sousa <joaovfsousa@gmail.com>
2024-07-30 22:49:05 -0300

    nvim(wk): finish migration of wk keys

--- config/nvim/lua/joaovfsousa/keymaps/wk/init.lua
+++ config/nvim/lua/joaovfsousa/keymaps/wk/init.lua
@@ -1,10 +1,10 @@
+require("joaovfsousa.keymaps.wk.debug")
 require("joaovfsousa.keymaps.wk.find")
-require("joaovfsousa.keymaps.wk.search")
-require("joaovfsousa.keymaps.wk.jester")
-require("joaovfsousa.keymaps.wk.dap")
 require("joaovfsousa.keymaps.wk.git")
 require("joaovfsousa.keymaps.wk.lsp")
 require("joaovfsousa.keymaps.wk.refactoring")
+require("joaovfsousa.keymaps.wk.search")
+require("joaovfsousa.keymaps.wk.test")
 
 local wk = require("which-key")
 
@@ -15,6 +15,16 @@ wk.add({
     desc = "Kill buffer",
   },
   {
+    "<leader>C",
+    "<cmd>pc<cr>",
+    desc = "Kill preview windows",
+  },
+  {
+    "<leader>D",
+    "<cmd>DBUIToggle<cr>",
+    desc = "Dadbod UI",
+  },
+  {
     "<leader>e",
     "<cmd>NvimTreeToggle<cr>",
     desc = "Toggle NvimTree",
@@ -23,5 +33,36 @@ wk.add({
     "<leader>q",
     "<cmd>q<cr>",
     desc = "Close window",
+  },
+  {
+    "<leader>w",
+    "<cmd>w<cr>",
+    desc = "Save",
+  },
+  {
+    "<leader>,",
+    "<cmd>lua require('chartoggle').toggle(',')<CR>",
+    desc = "Toggle comma",
+  },
+  {
+    "<leader>;",
+    "<cmd>lua require('chartoggle').toggle(';')<CR>",
+    desc = "Toggle semicolon",
+  },
+  {
+    mode = "n",
+    {
+      "<leader>/",
+      "<Plug>(comment_toggle_linewise_current)",
+      desc = "Comment toggle current line",
+    },
+  },
+  {
+    mode = "v",
+    {
+      "<leader>/",
+      "<Plug>(comment_toggle_linewise_visual)",
+      desc = "Comment toggle linewise (visual)",
+    },
   },
 })
