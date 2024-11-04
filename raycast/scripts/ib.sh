ca16387e266fa6cf9af3f7e3ed875b9d3f2cd0ca
João Vitor Sousa <joaovfsousa@gmail.com>
2024-11-04 00:03:55 -0300

    feat(raycast): quick incognito browser

---
+++ raycast/scripts/ib.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title IB
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName Browser
+
+open -a "Brave Browser.app" -n --args --incognito
+
