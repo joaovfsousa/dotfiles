45b49b6777eb7b81a6c6a0599e8e88a53a848119
João Vitor Sousa <joaovfsousa@gmail.com>
2024-11-27 15:46:55 -0300

    nvim: add toggle to textwrap

--- config/nvim/lua/joaovfsousa/config.lua
+++ config/nvim/lua/joaovfsousa/config.lua
@@ -1,6 +1,7 @@
 local config = {
   diagnostic = true,
   severity = vim.diagnostic.severity.HINT,
+  textwidth = 0,
 }
 
 return config
--- config/nvim/lua/joaovfsousa/keymaps/toggle.lua
+++ config/nvim/lua/joaovfsousa/keymaps/toggle.lua
@@ -8,9 +8,17 @@ wk.add({
     group = "Toggle",
   },
   {
+    "<leader>xd",
+    group = "Diagnostics",
+  },
+  {
+    "<leader>xl",
+    group = "LSP",
+  },
+  {
     mode = { "n", "v" },
     {
-      "<leader>xd",
+      "<leader>xda",
       function()
         config.diagnostic = not config.diagnostic
         vim.diagnostic.enable(config.diagnostic)
@@ -18,7 +26,7 @@ wk.add({
       desc = "Diagnostic",
     },
     {
-      "<leader>xw",
+      "<leader>xdw",
       function()
         if config.severity == vim.diagnostic.severity.ERROR then
           config.severity = vim.diagnostic.severity.HINT
@@ -41,11 +49,24 @@ wk.add({
       desc = "Diagnostic warnings",
     },
     {
-      "<leader>xi",
+      "<leader>xli",
       function()
         vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({}))
       end,
       desc = "Toggle Inlay Hints",
+    },
+    {
+      "<leader>xw",
+      function()
+        if config.textwidth == 0 then
+          vim.cmd("set textwidth=80")
+          config.textwidth = 80
+        else
+          vim.cmd("set textwidth=0")
+          config.textwidth = 0
+        end
+      end,
+      desc = "Toggle Text Wrap",
     },
   },
 })
