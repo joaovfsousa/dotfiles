f04073e53a105beaaf2eee1d899aac245d212cdb
João Vitor Sousa <joaovfsousa@gmail.com>
2025-01-08 21:57:25 -0300

    raycast: open work comns

---
+++ raycast/scripts/work-comns.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Work Comns
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 💼
+# @raycast.packageName WorkComns
+
+# Documentation:
+# @raycast.description Open Slack and Teams
+
+open /Applications/Slack.app
+sleep .5
+open /Applications/Microsoft\ Teams.app
+
