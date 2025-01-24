5693c3493aacbbda21fd37ff06bfcdfd4a4d182b
João Vitor Sousa <joaovfsousa@gmail.com>
2025-01-24 11:45:15 -0300

    raycast: script to open discord and telegram

---
+++ raycast/scripts/personal-comns.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Personal Comns
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 💬
+# @raycast.packageName PersonalComns
+
+# Documentation:
+# @raycast.description Open discord and telegram
+
+open /Applications/Discord.app
+sleep .5
+open /Applications/Telegram.app
+
