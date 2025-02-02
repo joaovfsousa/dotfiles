b5358395921276c80a8897a986828b9601dcd94b
João Vitor Sousa <joaovfsousa@gmail.com>
2025-02-01 21:13:03 -0300

    git: move git commands to git alias

--- zsh/lib/functions/git.zsh
+++ zsh/lib/functions/git.zsh
@@ -13,28 +13,14 @@ postpush() {
   fi
 }
 
-function commit_msg {
-  git log --format=%B -n 1 $1
-}
-
-function last_commit {
-  if git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
-    hash=$(g log -1 --oneline | awk '{print $1}')
-    echo $hash | pbcopy && echo "Commit $hash copied to clipboard"
-  else
-    echo "Not a git repository"
-    return 1
-  fi
-}
-
 function last_commit_msg {
   if git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
     commit_msg=$(git log -1 --pretty=format:%B | sed -e 's/^/ /')
 
-    echo $commit_msg | pbcopy && echo "Commit message copied to clipboard"
+    echo $commit_msg
   else
     echo "Not a git repository"
-    return 1
+    exit 1
   fi
 }
 
