93ea5739bda57b3303061f28d599ec65afaa38b8
João Vitor Sousa <joaovfsousa@gmail.com>
2025-02-05 07:51:50 -0300

    nvim: config fastaction to include lsp info

--- config/nvim/lua/joaovfsousa/plugins/local-plugins.lua
+++ config/nvim/lua/joaovfsousa/plugins/local-plugins.lua
@@ -3,20 +3,18 @@ return {
     dir = "/Users/joaovfsousa/projects/nvim-plugins/fastaction.nvim",
     name = "fastaction",
     enabled = true,
+    ---@module "fastaction"
     ---@type FastActionConfig
     opts = {
       dismiss_keys = { "<esc>" },
       keys = "asdfghjklqwertyuiop",
       priority = {
         typescript = {
-          { pattern = "add import from", order = 1 },
+          { pattern = "add import from", order = 1, key = "a" },
         },
       },
-      format_item = function(item)
-        return item.action.title
-          .. " ["
-          .. vim.lsp.get_client_by_id(item.ctx.client_id).name
-          .. "]"
+      format_right_section = function(item)
+        return "[" .. vim.lsp.get_client_by_id(item.ctx.client_id).name .. "]"
       end,
     },
   },
