5ccf0afc6758b5f13222d36bc44415e0d266cc62
João Vitor Sousa <joaovfsousa@gmail.com>
2025-02-26 21:06:22 -0300

    nvim: add tokyonight theme

--- config/nvim/lua/joaovfsousa/plugins/appearance.lua
+++ config/nvim/lua/joaovfsousa/plugins/appearance.lua
@@ -7,6 +7,12 @@ return {
     priority = 1000,
   },
   {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 10000,
