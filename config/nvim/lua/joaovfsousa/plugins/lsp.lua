f12e7cecece9edf5be2bc49542d7e94d8140db6a
João Vitor Sousa <joaovfsousa@gmail.com>
2026-05-10 23:21:02 -0300

    nvim: refactor lsp config + remove linting and formatting

--- config/nvim/lua/joaovfsousa/plugins/lsp.lua
+++ config/nvim/lua/joaovfsousa/plugins/lsp.lua
@@ -32,125 +32,67 @@ return {
           "pylsp",
           "rust_analyzer",
           "tailwindcss",
+          "ts_ls",
           "yamlls",
         },
       })
 
-      -- mason_lspconfig.setup_handlers({
-      --   function(server_name)
-      --     require("lspconfig")[server_name].setup({})
-      --   end,
-      --   ["tailwindcss"] = function()
-      --     local on_attach = function(_, bufnr)
-      --       require("tailwindcss-colors").buf_attach(bufnr)
-      --     end
-      --     require("lspconfig")["tailwindcss"].setup({
-      --       on_attach = on_attach,
-      --     })
-      --   end,
-      --   ["ts_ls"] = function()
-      --     -- Should not init. It's being installed to be used by "pmizio/typescript-tools.nvim"
-      --     -- in js projects
-      --   end,
-      --   ["jsonls"] = function()
-      --     local capabilities = vim.lsp.protocol.make_client_capabilities()
-      --     capabilities.textDocument.completion.completionItem.snippetSupport =
-      --       true
-      --     require("lspconfig").jsonls.setup({
-      --       capabilities = capabilities,
-      --       settings = {
-      --         json = {
-      --           schemas = require("schemastore").json.schemas(),
-      --           validate = { enable = true },
-      --         },
-      --       },
-      --     })
-      --   end,
-      --   ["yamlls"] = function()
-      --     require("lspconfig").yamlls.setup({
-      --       settings = {
-      --         yaml = {
-      --           schemaStore = {
-      --             -- You must disable built-in schemaStore support if you want to use
-      --             -- this plugin and its advanced options like `ignore`.
-      --             enable = false,
-      --             -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
-      --             url = "",
-      --           },
-      --           schemas = {
-      --             kubernetes = "*.yaml",
-      --             ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
-      --             ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-      --             ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
-      --             ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
-      --             ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
-      --             ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
-      --             ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
-      --             ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
-      --             ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
-      --             ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
-      --             ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
-      --             ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
-      --           },
-      --         },
-      --       },
-      --     })
-      --   end,
-      -- })
+      vim.lsp.config("tailwindcss", {
+        on_attach = function(_, bufnr)
+          require("tailwindcss-colors").buf_attach(bufnr)
+        end
+      })
+
+      local jsonls_capabilities = vim.lsp.protocol.make_client_capabilities()
+      jsonls_capabilities.textDocument.completion.completionItem.snippetSupport =
+          true
+
+      vim.lsp.config("jsonls", {
+        capabilities = jsonls_capabilities,
+        settings = {
+          json = {
+            schemas = require("schemastore").json.schemas(),
+            validate = { enable = true },
+          },
+        },
+      })
+
+      vim.lsp.config("yamlls", {
+        settings = {
+          yaml = {
+            schemaStore = {
+              -- You must disable built-in schemaStore support if you want to use
+              -- this plugin and its advanced options like `ignore`.
+              enable = false,
+              -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+              url = "",
+            },
+            schemas = {
+              kubernetes = "*.yaml",
+              ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+              ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+              ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+              ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+              ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+              ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
+              ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+              ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+              ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
+              ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] =
+              "*api*.{yml,yaml}",
+              ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] =
+              "*docker-compose*.{yml,yaml}",
+              ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] =
+              "*flow*.{yml,yaml}",
+            },
+          },
+        },
+      })
 
       vim.diagnostic.config({
         float = { border = "rounded" },
       })
     end,
-  },
-  {
-    "nvimtools/none-ls.nvim",
-    lazy = true,
-    dependencies = {
-      "nvimtools/none-ls-extras.nvim",
-    },
-    config = function()
-      local null_ls = require("null-ls")
-
-      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-      null_ls.setup({
-        on_attach = function(client, bufnr)
-          if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              group = augroup,
-              buffer = bufnr,
-              callback = function()
-                vim.lsp.buf.format({ async = false })
-              end,
-            })
-          end
-        end,
-      })
-    end,
-  },
-  {
-
-    "jayp0521/mason-null-ls.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      "williamboman/mason.nvim",
-      "nvimtools/none-ls.nvim",
-    },
-    opts = {
-      -- Available tools: https://github.com/jay-babu/mason-null-ls.nvim#available-null-ls-sources
-      ensure_installed = {
-        "stylua",
-        "prettier",
-        "goimports-reviser",
-        "gofumpt",
-        "yamlfmt",
-      },
-      automatic_setup = true,
-      automatic_installation = true,
-      handlers = {},
-    },
   },
   {
     "folke/lazydev.nvim",
@@ -167,44 +109,5 @@ return {
   {
     "smjonas/inc-rename.nvim",
     opts = {},
-  },
-  {
-    "pmizio/typescript-tools.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    enabled = false,
-    opts = {
-      on_attach = function(_, bufnr)
-        local filename = vim.api.nvim_buf_get_name(bufnr)
-
-        if not filename:match(".tsx$") then
-          return
-        end
-
-        local wk = require("which-key")
-        wk.add({
-          {
-            "gd",
-            "<cmd>TSToolsGoToSourceDefinition<cr>",
-            desc = "TS Goto definition",
-            buffer = bufnr,
-          },
-        })
-      end,
-      settings = {
-        separate_diagnostic_server = true,
-        expose_as_code_action = "all",
-        tsserver_file_preferences = {
-          importModuleSpecifierPreference = "non-relative",
-          includeInlayFunctionLikeReturnTypeHints = true,
-          includeInlayVariableTypeHints = true,
-          includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all'
-          includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-          includeInlayFunctionParameterTypeHints = true,
-          includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-          includeInlayPropertyDeclarationTypeHints = true,
-          includeInlayEnumMemberValueHints = false,
-        },
-      },
-    },
   },
 }
--- config/nvim/lua/joaovfsousa/plugins/qf.lua
+++ config/nvim/lua/joaovfsousa/plugins/qf.lua
@@ -1,21 +1,18 @@
 return {
   {
-    dir = "/Users/joaovfsousa/projects/nvim-plugins/fastaction.nvim",
+    "Chaitanyabsprip/fastaction.nvim",
     name = "fastaction",
     enabled = true,
     ---@module "fastaction"
     ---@type FastActionConfig
     opts = {
       dismiss_keys = { "<esc>" },
-      keys = "asdfghjklqwertyuiop",
+      keys = "asdghjklqwertyuiop",
       priority = {
         typescript = {
           { pattern = "add import from", order = 1, key = "a" },
         },
       },
-      format_right_section = function(item)
-        return "[" .. vim.lsp.get_client_by_id(item.ctx.client_id).name .. "]"
-      end,
     },
   },
   {
