5946d47e89719223d079a4b4d8d73c5bfc2e61dc
João Vitor Sousa <joaovfsousa@gmail.com>
2026-05-25 05:19:23 -0300

    raycast: add scripts

---
+++ manual/raycast_scripts/android-emulator.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Android Emulator
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 📱
+
+~/Library/Android/sdk/emulator/emulator -avd default
---
+++ manual/raycast_scripts/ib.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title IB
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName Browser
+
+open -a "Brave Browser.app" -n --args --incognito
